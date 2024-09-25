df98e4d39c1b2708e163ff491b1887e013637abc
dacioms <dacioms@gmail.com>
2024-09-25 20:37:23 -0300

    texto do commit

--- miniProjetoTurma/main.py
+++ miniProjetoTurma/main.py
@@ -75,7 +75,16 @@ def cadastrar_aluno():
     print("Aluno cadastrado com sucesso!")
     # print(lista_alunos)
 
-def calcular_medias(port, mat, cien):
+def calcular_medias(port : list, mat : list, cien : list) -> list:
+    '''Função que calcula a média das notas de um aluno
+    
+    Args:
+        port (list): lista de notas do aluno em português
+        mat (list): lista de notas do aluno em matemática
+        cien (list): lista de notas do aluno em ciências
+
+    return: lista de notas do aluno em português, matemática e ciências
+    '''
     media_port = sum(port) / len(port)
     media_mat = sum(mat) / len(mat)
     media_cien = sum(cien) / len(cien)
